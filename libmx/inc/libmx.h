b331a817726ab846f35b501b41f034cb2935f4a9
KyryloYehorov <Kyrylo.Yehorov@cit.khpi.edu.ua>
2023-05-03 21:18:00 +0300

    main

--- libmx/inc/libmx.h
+++ libmx/inc/libmx.h
@@ -17,7 +17,7 @@ void mx_push_back(t_list **list, void *data);
 void mx_pop_back(t_list **head);
 void mx_pop_front(t_list **head);
 int mx_list_size(t_list *list);
-t_list *mx_sort_list(t_list *list, bool (*cmp)(void *a, void *b));
+t_list *mx_sort_list(t_list *lst, int (*cmp)(void *a, void *b));
 void mx_clear_list(t_list **list);
 t_list *mx_list_last(t_list *list);
 
